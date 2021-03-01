7836fe7606d44b86fffce13a504c7ee508d59eea
Vladimir Riz <resin.fhp@gmail.com>
2021-03-01 16:05:59 +0300

    feat/extracting dynamics params

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,6 +24,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log(prodId);
+  res.redirect('/');
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', { pageTitle: 'Orders', path: '/orders' });
 };
