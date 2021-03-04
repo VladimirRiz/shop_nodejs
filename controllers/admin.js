a5649fdfb52970b0a786a0a870df348463983c61
Vladimir Riz <resin.fhp@gmail.com>
2021-03-04 22:30:04 +0300

    feat: update product

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,11 +10,10 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const { name, imageURL, price, desc } = req.body;
-  Product.create({ title: name, price: price, imageUrl: imageURL, desc: desc })
-    .then((res) => {
-      console.log(res);
-      // res.redirect('/');
+  const { title, imageUrl, price, desc } = req.body;
+  Product.create({ title: title, price: price, imageUrl: imageUrl, desc: desc })
+    .then((result) => {
+      res.redirect('/admin/product');
     })
     .catch((err) => console.log(err));
 };
@@ -23,21 +22,32 @@ exports.getEditProduct = (req, res, next) => {
   const isEditing = req.query.edit;
   if (!isEditing) res.redirect('/');
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/edit-product',
-      product: product,
-      editing: isEditing,
-    });
-  });
+  Product.findByPk(prodId)
+    .then((product) => {
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/edit-product',
+        product: product,
+        editing: isEditing,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const { id, name, imageURL, price, desc } = req.body;
-  const updatedProduct = new Product(id, name, imageURL, desc, price);
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  const { id, title, imageUrl, price, desc } = req.body;
+  Product.findByPk(id)
+    .then((product) => {
+      product.title = title;
+      product.imageUrl = imageUrl;
+      product.price = price;
+      product.desc = desc;
+      return product.save();
+    })
+    .then((result) => {
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
