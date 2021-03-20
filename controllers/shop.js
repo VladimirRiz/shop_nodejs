25f56b890cf91d6ad7cc58892742efa170c0e5cd
Vladimir Riz <resin.fhp@gmail.com>
2021-03-20 12:38:05 +0300

    download files with authentication

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,9 @@ const { findById } = require('../models/product');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const fs = require('fs');
+const path = require('path');
+
 exports.getIndex = (req, res, next) => {
   Product.find().then((products) => {
     res.render('shop/index', {
@@ -136,6 +139,18 @@ exports.postOrder = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.send(data);
+  });
 };
 
 // exports.getCheckout = (req, res, next) => {
